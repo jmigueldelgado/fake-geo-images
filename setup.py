531e38e8fb1a60dbf827d57f15d0e151ed23baeb
Markus Müller <markus.mueller@up42.com>
2020-10-15 08:56:11 +0200

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,18 @@
-import os
-from pathlib import Path
 import setuptools
-
-PARENT_DIR = Path(__file__).resolve().parent
-
-
-def set_directory():
-    # CD to this directory, to simplify package finding
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    os.chdir(dname)
-
-
-set_directory()
 
 with open("docs/index.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
     name="fake-geo-images",
-    version=PARENT_DIR.joinpath("fake-geo-images/_version.txt").read_text(
-        encoding="utf-8"
-    ),
+    version="0.1.0",
     author="UP42",
     author_email="support@up42.com",
     description="Fake geospatial images for unit tests",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://www.up42.com",
-    packages=setuptools.find_packages(exclude=("tests")),
+    url="https://github.com/up42/fake-geo-images",
+    packages=setuptools.find_packages(exclude=("test")),
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
