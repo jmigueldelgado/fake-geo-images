0812fa1bbe673ba8aad0362b090d6c03da6fb6bf
Christoph Rieke <christoph.rieke@up42.com>
2021-09-20 19:54:17 +0200

    specify encoding in txt reading

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="UP42",
     author_email="support@up42.com",
     description="Fake geospatial images for unit tests",
-    long_description=parent_dir.joinpath("README.md").read_text(),
+    long_description=parent_dir.joinpath("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/up42/fake-geo-images",
     packages=find_packages(),
@@ -21,6 +21,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
     ],
-    install_requires=parent_dir.joinpath("requirements.txt").read_text().splitlines(),
+    install_requires=parent_dir.joinpath("requirements.txt").read_text(encoding="utf-8").splitlines(),
     python_requires=">=3.6, <3.9",
 )
